0c4267837d2de1003ad006a75c0e48473049edaa
njoroge-s <sarahnjoroge2015@gmail.com>
2023-04-19 13:32:29 +0300

    fix

--- 0x01-caching/1-fifo_cache.py
+++ 0x01-caching/1-fifo_cache.py
@@ -1,8 +1,6 @@
 #!/usr/bin/env python3
 
-from collections import OrderedDict
-from base_caching import BaseCaching
-
+BaseCaching = __import__('base_caching').BaseCaching
 
 class FIFOCache(BaseCaching):
     '''A class `FIFOCache` that inherits from
